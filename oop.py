5702bc4d19a26cd8a2e11d061e6a0cbbadc6c00a
goldenglorys <olusolagloryolamide@gmail.com>
2021-04-13 16:53:21 +0100

    Witdrawing function added for all category

--- oop.py
+++ oop.py
@@ -15,8 +15,18 @@ class Budget:
             self.entertainment_balance += int(amount)
             return "Deposited amount to entertainment budget: " + str(amount) + ". Total balance: " + str(self.entertainment_balance)
             
-   
-
+    def witdrawlFund(self, category, amount):
+        if category == 'food':
+            self.food_balance -= int(amount)
+            return "Withdrawl amount to food budget: "  + str(amount) + ". Total balance: " + str(self.food_balance)
+        elif category == 'clothing':
+            self.clothing_balance -= int(amount)
+            return "Withdrawl amount to clothing budget: " + str(amount) + ". Total balance: " + str(self.clothing_balance)
+        elif category == 'entertainment':
+            self.entertainment_balance -= int(amount)
+            return "Withdrawl amount to entertainment budget: "+ str(amount) + ". Total balance: " + str(self.entertainment_balance)
+        
+        
 food = Budget()
 
 clothing = Budget()
@@ -27,3 +37,6 @@ print(food.depositFund('food', 10))
 print(clothing.depositFund('clothing', 50))
 print(entertainment.depositFund('entertainment', 80))
 
+print(food.witdrawlFund('food', 10))
+print(clothing.witdrawlFund('clothing', 50))
+print(entertainment.witdrawlFund('entertainment', 80))
