caa07db6d91dfcf90b271e031a5cc223db413eff
goldenglorys <olusolagloryolamide@gmail.com>
2021-04-13 23:06:17 +0100

    Code block splitted into function blocks

--- atm.py
+++ atm.py
@@ -17,29 +17,41 @@ allowedUsers = ["Zenith", "Edward"]
 allowedPassword = ["passwordZenith", "passwordEdward"]
 accountBalance = 0
 
+def withdrawl():
+    withdrawlAmount = input("How much would you want to witdrawl?")
+    print("Take cash of %s " % withdrawlAmount)
+
+def deposit():
+    depositAmount = float(input("How much would you like to deposit?"))
+    totalBalance = accountBalance + depositAmount
+    print("Total account balance: %s " % totalBalance)
+
+def complaint():
+    issue = input("What issue will you like to report? ")
+    print("Thank you for contacting us! Our support team would reach out to you soon.")
+
+def loginInfo():
+    print("Welcome %s" % name)
+    print("Login date and time is %s" % datetime.datetime.now())
+    print("These are the available options:")
+    print("1. Withdrawal")
+    print("2. Deposit")
+    print("3. Complaint")
+    
 if name in allowedUsers:
     password = input("What is your password?\n")
     userId = allowedUsers.index(name)
     if password in allowedPassword[userId]:
-        print("Welcome %s" % name)
-        print("Login date and time is %s" % datetime.datetime.now())
-        print("These are the available options:")
-        print("1. Withdrawal")
-        print("2. Deposit")
-        print("3. Complaint")
+        loginInfo()
 
         selectedOption = input("Please select an option:")
 
         if selectedOption == '1':
-            withdrawlAmount = input("How much would you want to witdrawl?")
-            print("Take cash of %s " % withdrawlAmount)
+            withdrawl()
         elif selectedOption == '2':
-            depositAmount = float(input("How much would you like to deposit?"))
-            totalBalance = accountBalance + depositAmount
-            print("Total account balance: %s " % totalBalance)
+            deposit()
         elif selectedOption == '3':
-            issue = input("What issue will you like to report? ")
-            print("Thank you for contacting us! Our support team would reach out to you soon.")
+           complaint()
              
     else:
         print("Password Incorrect, please try again")
