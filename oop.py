e017e8850e6c5918a5825ca1f8976899861b4b98
goldenglorys <olusolagloryolamide@gmail.com>
2021-04-13 16:49:20 +0100

    Created a budget app with multiple category using oop and added a deposit fund functions for each category

---
+++ oop.py
@@ -0,0 +1,29 @@
+class Budget:
+    def __init__(self):
+        self.food_balance = 100
+        self.clothing_balance = 100
+        self.entertainment_balance = 100
+
+    def depositFund(self, category, amount):
+        if category == 'food':
+            self.food_balance += int(amount)
+            return "Deposited amount to food budget: " + str(amount) + ". Total balance: " + str(self.food_balance)
+        elif category == 'clothing':
+            self.clothing_balance += int(amount)
+            return "Deposited amount to clothing budget: " + str(amount) + ". Total balance: " + str(self.clothing_balance)
+        elif category == 'entertainment':
+            self.entertainment_balance += int(amount)
+            return "Deposited amount to entertainment budget: " + str(amount) + ". Total balance: " + str(self.entertainment_balance)
+            
+   
+
+food = Budget()
+
+clothing = Budget()
+
+entertainment = Budget()
+
+print(food.depositFund('food', 10))
+print(clothing.depositFund('clothing', 50))
+print(entertainment.depositFund('entertainment', 80))
+
