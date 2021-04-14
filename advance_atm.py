ffbbc82350cd887c540ba14322c06274ac709da1
goldenglorys <olusolagloryolamide@gmail.com>
2021-04-14 08:28:36 +0100

    Cleared comments

--- advance_atm.py
+++ advance_atm.py
@@ -1,14 +1,3 @@
-# ===================================================================================================
-# Improve on your ATM mockup from last course to include the following:
-
-# 1. Use functions
-
-# 2. Include register, and login
-
-# 3. Generate Account Number
-
-# 4. Any other improvement you can think of (extra point)
-
 import datetime
 import random
 
