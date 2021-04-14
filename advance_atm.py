b68a3554c69e34750772a5d7b34feaec28af73bc
goldenglorys <olusolagloryolamide@gmail.com>
2021-04-14 11:53:16 +0100

    Same bank trasfer implemented

--- advance_atm.py
+++ advance_atm.py
@@ -36,6 +36,19 @@ def complaint():
 def generateAccountNumber():
     return random.randint(1111111111, 9999999999)
 
+def transferWithin():
+    from_account = int(input("Enter account number to trasfer from: \n"))
+    to_account = int(input("Enter account number to transfer to: \n"))
+    amount = int(input("Enter amount: \n"))
+    description = input("Description: \n")
+    for accountNumber, userDetails in database.items():
+        print(accountNumber)
+        if accountNumber == from_account:
+            print("%s on it way to %s." % (amount, to_account))
+        else:
+            print("Invalid account number, please try again")
+            transferWithin()
+
 def register():
     print("========== Account Opening Form =========")
     email = input("Wht is your email? \n")
@@ -56,7 +69,8 @@ def bankOperation(user):
     print("Welcome %s %s. These are the available options:" % ( user[0],  user[1] ))
     print("1. Withdrawal")
     print("2. Deposit")
-    print("3. Complaint")
+    print("3. Same bank trasfer")
+    print("4. Complaint")
     selectedOption = input("Please select an option:")
 
     if selectedOption == '1':
@@ -64,6 +78,8 @@ def bankOperation(user):
     elif selectedOption == '2':
         deposit()
     elif selectedOption == '3':
+        transferWithin()
+    elif selectedOption == '4':
         complaint()
     else:
         print("Invalid option selected")
